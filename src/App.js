af1542ab891f3a182b07ae8a31578c199de77408
Munkh-Od <eikyuu.hoshi@gmail.com>
2020-08-22 08:00:00 +0800

    Added Pages Structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
 
+// Pages
+import Home from './pages/Home'
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Home />
     </div>
   );
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import './Home.css';
+
+function Home(){
+  return (			
+    <div className='Home'>
+      <h1>Home Page</h1>
+
+      <div className="home__header">
+        <div className="home__headerLeft">
+        </div>
+
+        <div className="home__headerRight">
+        </div>
+      </div>
+
+      <div className="home__body">
+      </div>
+
+    </div>
+  )
+}
+
+export default Home
